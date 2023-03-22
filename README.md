96eb52be01b38b5671e913e5fe4c1c28d27a22fe
SherlySQL <sherly.clarke@gmail.com>
2023-03-22 05:37:25 -0600

    Readme Agregado

---
+++ README.md
@@ -0,0 +1,8 @@
+#GifExpertApp
+
+Este es mi respositorio del proyecto de GifExpertApp de React
+
+
+Tecnologias utilizadas:
+
+Vite, React, React Testing Library
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifts = async( category ) =>{ //Función que nos trae el Api
 
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=2xjwU3DCVt9IBoH0E1dzSsnMeAQYivBE&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=2xjwU3DCVt9IBoH0E1dzSsnMeAQYivBE&q=${ category }&limit=20`;
     const resp = await fetch ( url );
     const { data } = await resp.json();
   
